dce42d420e9788954a34c0ee71a5d8dd8636b7e1
godwin <your_mail>
2026-04-22 12:48:58 +0100

    refactor: refactor layout component by removing unused font imports and simplifying the HTML structure

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,33 +1,20 @@
 import type { Metadata } from "next";
-import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
 
-const geistSans = Geist({
-  variable: "--font-geist-sans",
-  subsets: ["latin"],
-});
-
-const geistMono = Geist_Mono({
-  variable: "--font-geist-mono",
-  subsets: ["latin"],
-});
-
 export const metadata: Metadata = {
-  title: "ShopNext",
-  description: "Welcome to ShopNext, your one-stop destination for all your shopping needs. Explore our wide range of products and enjoy a seamless shopping experience.",
+    title: "ShopNext",
+    description:
+        "Welcome to ShopNext, your one-stop destination for all your shopping needs. Explore our wide range of products and enjoy a seamless shopping experience.",
 };
 
 export default function RootLayout({
-  children,
+    children,
 }: Readonly<{
-  children: React.ReactNode;
+    children: React.ReactNode;
 }>) {
-  return (
-    <html
-      lang="en"
-      className={`${geistSans.variable} ${geistMono.variable} h-full antialiased`}
-    >
-      <body className="min-h-full flex flex-col">{children}</body>
-    </html>
-  );
+    return (
+        <html lang="en">
+            <body>{children}</body>
+        </html>
+    );
 }
