096dd26128f982127a27a50ba260977d5b82a97f
godwin <your_mail>
2026-04-22 17:14:39 +0100

    feat: replace main with Container component in Home for improved layout consistency

--- app/page.tsx
+++ app/page.tsx
@@ -1,22 +1,24 @@
-import React from "react";
+import Container from "@/components/Container";
 
 const Home = () => {
     return (
-        <main className="p-10">
-            <h1 className="text-3xl font-bold text-dark">
-                Welcome to ShopNext
-            </h1>
-            <p className="text-gray-500 mt-2">
-                Discover the latest products at unbeatable prices.
-            </p>
-            <div className="flex gap-3 mt-6">
-                <button className="bg-primary text-white px-6 py-2 rounded-full text-sm font-medium">
-                    Shop Now
-                </button>
-                <button className="bg-navy text-white px-6 py-2 rounded-full text-sm font-medium">
-                    Learn More
-                </button>
-            </div>
+        <main >
+            <Container className="py-10">
+                <h1 className="text-3xl font-bold text-dark">
+                    Welcome to ShopNext
+                </h1>
+                <p className="text-gray-500 mt-2">
+                    Discover the latest products at unbeatable prices.
+                </p>
+                <div className="flex gap-3 mt-6">
+                    <button className="bg-primary text-white px-6 py-2 rounded-full text-sm font-medium">
+                        Shop Now
+                    </button>
+                    <button className="bg-navy text-white px-6 py-2 rounded-full text-sm font-medium">
+                        Learn More
+                    </button>
+                </div>
+            </Container>
         </main>
     );
 };
